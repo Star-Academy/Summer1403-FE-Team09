b22a01e1cc2a89ab62444289593491005fbcd9a8
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-05 01:45:42 +0330

    refactor aside and create Add Book form

--- Phase06/p6/src/app/component/aside/aside.component.ts
+++ Phase06/p6/src/app/component/aside/aside.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { ApiService } from '../../service/api.service';
+import Book from '../../interface/book';
 
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './aside.component.html',
-  styleUrl: './aside.component.scss'
+  styleUrl: './aside.component.scss',
 })
 export class AsideComponent {
 
+  constructor(public api: ApiService) {
+  }
+
+  bookForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+    genre: new FormControl('', Validators.required),
+    author: new FormControl('', Validators.required),
+    publishData: new FormControl('', Validators.required),
+    price: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")]),
+  });
+
+  onSubmit() {
+    if (this.bookForm.valid) {
+
+      const new_book: Book = {
+        name: this.bookForm.value.name || "name",
+        image: this.bookForm.value.image || "image",
+        genre: this.bookForm.value.genre?.split(',') || [],
+        author: this.bookForm.value.author || "author",
+        publishData: this.bookForm.value.publishData || "publishData",
+        price: Number(this.bookForm.value.price),
+      };
+
+      this.api.addBook(new_book);
+      console.log("Form Submitted!");
+    }
+    else {
+      alert("Please fill all the fields!");
+    }
+  }
 }
