9bec520128eb86fd60b10fab098497c4dcf64172
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-04 22:38:26 +0330

    use API Service in Main Component :fax:

--- Phase06/p6/src/app/component/main/main.component.ts
+++ Phase06/p6/src/app/component/main/main.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../service/api.service';
+import Book from '../../interface/book';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+
+  protected books?: Book[];
+  constructor(protected api: ApiService) {
+    api.getBooks().subscribe(books => {
+      this.books = books;
+    });
+  }
 
 }
