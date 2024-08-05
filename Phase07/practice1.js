0349347cdb1f9b4ee3bafec9957cfa4fa3b8ecd3
FrOZEn-FurY <h.nazarieh@gmail.com>
2024-08-05 19:48:46 +0330

    Practice one complete

--- Phase07/practice1.js
+++ Phase07/practice1.js
@@ -1,1 +1,10 @@
 import { of } from 'rxjs';
+
+const observable = of(1, 2, 3, 4, 5);
+const observer = {
+    next: x => console.log('Observer got a next value: ' + x),
+    error: err => console.error('Observer got an error: ' + err),
+    complete: () => console.log("Stream complete.")
+}
+
+observable.subscribe(observer)
