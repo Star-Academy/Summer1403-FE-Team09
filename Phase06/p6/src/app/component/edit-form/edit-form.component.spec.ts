73ebb29a6ec8336a6ad26342ad55cdd1f53476cf
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-09 22:16:41 +0330

    Update edit-form.component.spec.ts

--- Phase06/p6/src/app/component/edit-form/edit-form.component.spec.ts
+++ Phase06/p6/src/app/component/edit-form/edit-form.component.spec.ts
@@ -1,23 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EditFormComponent } from './edit-form.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('EditFormComponent', () => {
   let component: EditFormComponent;
   let fixture: ComponentFixture<EditFormComponent>;
-
+  let htmlDom: HTMLElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditFormComponent]
+      imports: [EditFormComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
-
     fixture = TestBed.createComponent(EditFormComponent);
     component = fixture.componentInstance;
+    htmlDom = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('it component SHOULD be created WHEN initial', () => {
+    expect(component).toBeDefined();
+  });
+  
+  it("form SHOULD be created WHEN initial", () => {
+    expect(htmlDom.querySelector('form')).toBeDefined();
+    expect(htmlDom.querySelector('form #name')).toBeDefined();
+    expect(htmlDom.querySelector('form #image-url')).toBeDefined();
+    expect(htmlDom.querySelector('form #genre')).toBeDefined();
+    expect(htmlDom.querySelector('form #author')).toBeDefined();
+    expect(htmlDom.querySelector('form #publishData')).toBeDefined();
+    expect(htmlDom.querySelector('form #price')).toBeDefined();
+    expect(htmlDom.querySelector('form button')).toBeDefined();
   });
 });
