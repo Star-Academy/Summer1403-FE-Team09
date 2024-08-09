59e8b175a1cfb592937ea026ae92a325cb56c731
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-09 22:46:10 +0330

    Added cypress into package.json

--- Phase06/p6/cypress.config.ts
+++ Phase06/p6/cypress.config.ts
@@ -1,7 +1,18 @@
-export default {
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    'baseUrl': 'http://localhost:4200'
   },
-};
+  
+  
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts'
+  }
+  
+})
