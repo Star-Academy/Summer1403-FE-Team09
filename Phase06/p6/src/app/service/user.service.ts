f275eb249ffa08a1bb0a6b48e52193e73ab1636f
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-11 23:33:38 +0330

    refactor user api (using rxjs) :ski:

--- Phase06/p6/src/app/service/user.service.ts
+++ Phase06/p6/src/app/service/user.service.ts
@@ -1,40 +1,42 @@
 import { Injectable } from '@angular/core';
-import { User } from "../interface/book";
 import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { User } from "../interface/book";
 import { USER_API_URL } from '../app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private user: User | undefined = undefined;
+  private obs = new BehaviorSubject<User | undefined>(undefined);
 
   constructor(private http: HttpClient) { }
 
-  getUser(): User | undefined {
-    return this.user;
+  subscribeUser(): Observable<User | undefined> {
+    return this.obs.asObservable();
   }
 
   logOut(): void {
-    this.user = undefined;
+    this.obs.next(undefined);
   }
 
-  loginUser(user: { email: string, password: string }): string {
-    let response: string = "";
-    this.http.get<User[]>(USER_API_URL).subscribe((users: User[]) => {
-      const u = users.find((u: User) => user.email === u.email);
-      if (!u) {
-        response = "User not found";
-        return;
-      } else if (u.password != user.password) {
-        response = "Passwords do not match";
-        return;
-      }
-      this.user = u;
-      response = "ok";
-    });
+  loginUser(user: { email: string, password: string }): Promise<string> {
 
-    return response;
+    return new Promise((resolve) => {
+      this.http.get<User[]>(USER_API_URL).subscribe((users: User[]) => {
+        const u = users.find((u: User) => user.email === u.email);
+        if (!u) {
+          resolve("User not found");
+          return;
+        } else if (u.password != user.password) {
+          resolve("Passwords do not match");
+          return;
+        }
+        this.obs.next(u);
+        resolve("ok");
+      });
+    });
   }
 
   signUpUser(user: {
@@ -49,7 +51,7 @@ export class UserService {
           if (!u) {
             resolve("User not found");
           } else {
-            this.user = u;
+            this.obs.next(u);
             resolve("ok");
           }
         }
