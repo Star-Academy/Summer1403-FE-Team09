a9b42474de0b0a369c0dda51745a9fb8ab8bbd62
FrOZEn-FurY <h.nazarieh@gmail.com>
2024-08-11 19:25:09 +0330

    Home component generated

--- Phase06/p6/src/app/app.routes.ts
+++ Phase06/p6/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router';
 import {EditFormComponent} from './component/edit-form/edit-form.component';
+import {HomeComponent} from "./component/home/home.component";
 
 export const routes: Routes = [
   {path: 'edit/:id', component: EditFormComponent},
+  {path: '', component: HomeComponent},
 ];
