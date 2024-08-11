2abc97fe16836c767c1b3ee6a768fdf40adbfd60
FrOZEn-FurY <h.nazarieh@gmail.com>
2024-08-11 22:51:32 +0330

    Login and register guards added

--- Phase06/p6/src/app/app.routes.ts
+++ Phase06/p6/src/app/app.routes.ts
@@ -5,11 +5,12 @@ import {SignupComponent} from "./component/signup/signup.component";
 import {LoginComponent} from "./component/login/login.component";
 import {ProfileComponent} from "./component/profile/profile.component";
 import {profileGuardGuard} from "./guards/profile-guard.guard";
+import {authenticatorGuardGuard} from "./guards/authenticator-guard.guard";
 
 export const routes: Routes = [
   {path: 'edit/:id', component: EditFormComponent},
   {path: '', component: HomeComponent},
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent, canActivate: [authenticatorGuardGuard]},
+  {path: 'login', component: LoginComponent, canActivate: [authenticatorGuardGuard]},
   {path: 'profile', component: ProfileComponent, canActivate: [profileGuardGuard]}
 ];
--- Phase06/p6/src/app/guards/authenticator-guard.guard.ts
+++ Phase06/p6/src/app/guards/authenticator-guard.guard.ts
@@ -1,5 +1,9 @@
 import { CanActivateFn } from '@angular/router';
+import {inject} from "@angular/core";
+import {UserService} from "../service/user.service";
 
 export const authenticatorGuardGuard: CanActivateFn = (route, state) => {
-  return true;
+  const service = inject(UserService);
+  const user = service.getUser();
+  return !user;
 };
